7f679e5b4f3b30132abe74543a75d9cce3d38e15
dark1knight <nakamuta26@gmail.com>
2023-06-03 08:41:16 -0400

    local weather being retrieved
    Local weather being sucessfully retrieved and displayed on the screen

--- src/GeolocationComponent.js
+++ src/GeolocationComponent.js
@@ -1,15 +1,18 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const GeolocationComponent = () => {
   const [latitude, setLatitude] = useState(null);
   const [longitude, setLongitude] = useState(null);
+  const [weather, setWeather] = useState(null);
 
   useEffect(() => {
     if ("geolocation" in navigator) {
       navigator.geolocation.getCurrentPosition(
-        (position) => {
+        async (position) => {
           setLatitude(position.coords.latitude);
           setLongitude(position.coords.longitude);
+          await fetchWeatherData(position.coords.latitude, position.coords.longitude);
         },
         (error) => {
           console.error("Error getting geolocation:", error);
@@ -20,15 +23,34 @@ const GeolocationComponent = () => {
     }
   }, []);
 
+  const fetchWeatherData = async (latitude, longitude) => {
+    const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m,weathercode`;
+  
+    try {
+      const response = await axios.get(url);
+      setWeather(response.data);
+      console.log(response.data); 
+    } catch (error) {
+      console.error("Error fetching weather data:", error);
+    }
+  };
+
   return (
     <div>
       {latitude && longitude ? (
         <>
-          Latitude: {latitude} <br />
-          Longitude: {longitude}
+          <h2>Weather at your location:</h2>
+          {weather ? (
+            <>
+              <p>Temperature: {weather.hourly.temperature_2m[0]}°C</p>
+              <p>Time: {weather.hourly.time[0]}</p>
+            </>
+          ) : (
+            <p>Loading weather data...</p>
+          )}
         </>
       ) : (
-        "Fetching geolocation..."
+        <p>Fetching geolocation...</p>
       )}
     </div>
   );
